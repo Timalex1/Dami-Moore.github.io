a18464b196fe8fa31ecd9f5cc89bdf1cd68e4c95
Timilehin Vincent <timilehinvincent@gmail.com>
2021-11-11 14:17:34 +0100

    Adding close toggle button functionality on click outside

--- portfolio.js
+++ portfolio.js
@@ -6,13 +6,22 @@ toggler.addEventListener('click', function (e) {
 
     e.preventDefault();
 
-    
     if (dropdown.style.display === 'block') {
         dropdown.style.display = 'none';
     } else {
         dropdown.style.display = 'block';
     }
 });
+
+// close the toggle button on outside div clicks
+
+document.addEventListener('click', (e) => {
+
+    if (!e.target.isEqualNode(menu) && !menu.contains(e.target)) {
+        dropdown.style.display = 'none';
+    }
+
+})
 
 
 
